cef2027f112c31db83c54e03624ac5833e512a3a
André Cooke <andrecooke@hotmail.com>
2015-11-23 08:01:18 -0500

    work on walls insert sheet - create list

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using  System;
 using  System.Reflection;
+using System.Collections.Generic;
 using  System.Runtime.InteropServices;
+using SolidEdgeDraft;
 
 namespace SE_interface
 {
@@ -13,8 +15,21 @@ namespace SE_interface
         static SolidEdgePart.PartDocument part = null;
         static Type type = null;
 
+        static SolidEdgeDraft.Sections sections = null;
+        static SolidEdgeDraft.Section section = null;
+        static SolidEdgeDraft.SectionSheets sectionSheets = null;
+        static SolidEdgeDraft.Sheets sheets = null;
+        static SolidEdgeDraft.Sheet sheet = null;
+        static string format1 = "Section = {0}";
+        static string format2 = "Sheet = {0}";
+
+        static List<string> ListeDesDraft =  new List<string> { "MUR A", "MUR B", "MUR C", "MUR D", "CHEVRONS" };
+
+        
+
         static void Main(string[] args)
         {
+            
             try
             {
                 // Get the type from the Solid Edge ProgID
@@ -28,10 +43,11 @@ namespace SE_interface
                 Console.WriteLine("Solid Edge non actif...");
                 StartSE();
             }
-            
+
 
             // faire la magie icitte
-            CreateDoc();
+            CreateDraft();
+            //CreateDoc();
 
         }
 
@@ -56,7 +72,7 @@ namespace SE_interface
 
 
         static void CleanSE()
-        { 
+        {
             if (application != null)
             {
                 Marshal.ReleaseComObject(application);
@@ -67,13 +83,109 @@ namespace SE_interface
 
         static void CreateDoc()
         {
-        Console.WriteLine("Creation de document test");
-        documents = application.Documents;
-        assembly = (SolidEdgeAssembly.AssemblyDocument)documents.Add("SolidEdge.AssemblyDocument", Missing.Value);
-        draft = (SolidEdgeDraft.DraftDocument)documents.Add("SolidEdge.DraftDocument", Missing.Value);
-        part = (SolidEdgePart.PartDocument)documents.Add("SolidEdge.PartDocument", Missing.Value);
-        CleanSE();
+            Console.WriteLine("Creation de document test");
+            documents = application.Documents;
+            assembly = (SolidEdgeAssembly.AssemblyDocument)documents.Add("SolidEdge.AssemblyDocument", Missing.Value);
+            draft = (SolidEdgeDraft.DraftDocument)documents.Add("SolidEdge.DraftDocument", Missing.Value);
+            part = (SolidEdgePart.PartDocument)documents.Add("SolidEdge.PartDocument", Missing.Value);
+            CleanSE();
+        }
+
+        static void CreateDraft()
+        {
+            //draft = (SolidEdgeDraft.DraftDocument)documents.Add("SolidEdge.DraftDocument", Missing.Value);
+            try
+            {
+                //add a draft document
+                documents = application.Documents;
+                draft = (SolidEdgeDraft.DraftDocument)documents.Add("SolidEdge.DraftDocument", Missing.Value);
+                
+                // Get a reference to the sheets collection
+                sheets = draft.Sheets;
+                
+                // Add sheets to draft document
+                // Loop thru list of wall - to do
+
+
+                // Add a new sheet
+                sheet = sheets.Item(1);
+                sheet.Activate();
+                sheet.Name = "MUR A";
+                // Add wall drawings
+
+                //Insert next wall
+                sheet = sheets.AddSheet("MUR B", SheetSectionTypeConstants.igWorkingSection, Missing.Value, Missing.Value);
+                sheet.Activate();
+                // Add wall drawing
+
+                //Insert next wall
+                sheet = sheets.AddSheet("MUR C", SheetSectionTypeConstants.igWorkingSection, Missing.Value, Missing.Value);
+                sheet.Activate();
+                // Add wall drawings
+
+                //Insert next wall
+                sheet = sheets.AddSheet("MUR D", SheetSectionTypeConstants.igWorkingSection, Missing.Value, Missing.Value);
+                sheet.Activate();
+                // Add wall drawings
+
+                //Insert next wall
+                sheet = sheets.AddSheet("CHEVRONS", SheetSectionTypeConstants.igWorkingSection, Missing.Value, Missing.Value);
+                sheet.Activate();
+                // Add wall drawings
+
+            }
+
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sheet != null)
+                {
+                    Marshal.ReleaseComObject(sheet);
+                    sheet = null;
+                }
+                if (sheets != null)
+                {
+                    Marshal.ReleaseComObject(sheets);
+                    sheets = null;
+                }
+                if (sectionSheets != null)
+                {
+                    Marshal.ReleaseComObject(sectionSheets);
+                    sectionSheets = null;
+                }
+                if (section != null)
+                {
+                    Marshal.ReleaseComObject(section);
+                    section = null;
+                }
+                if (draft != null)
+                {
+                    Marshal.ReleaseComObject(draft);
+                    draft = null;
+                }
+                if (documents != null)
+                {
+                    Marshal.ReleaseComObject(documents);
+                    documents = null;
+                }
+                if (application != null)
+                {
+                    Marshal.ReleaseComObject(application);
+                    application = null;
+                }
+
+
+            }
+
+
+
 
         }
+
+           
+        
     }
 }
